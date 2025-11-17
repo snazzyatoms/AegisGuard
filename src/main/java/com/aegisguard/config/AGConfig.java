e2cdfca60f4082e1f29a1056384cee1bdb211be5
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-11-17 00:54:07 -0700

    Update AGConfig.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/config/AGConfig.java
+++ src/main/java/com/aegisguard/config/AGConfig.java
@@ -16,6 +16,11 @@ public class AGConfig {
     private final AegisGuard plugin;
     private FileConfiguration cfg;
 
+    // --- NEW ---
+    // This version number should match the one in your default config.yml
+    // When you add new options, you bump this number.
+    private static final double LATEST_CONFIG_VERSION = 1.0;
+
     public AGConfig(AegisGuard plugin) {
         this.plugin = plugin;
         reload();
@@ -27,114 +32,176 @@ public class AGConfig {
     public void reload() {
         plugin.reloadConfig();
         this.cfg = plugin.getConfig();
+
+        // --- NEW ---
+        // Sync defaults to add any new options to the user's config
+        syncConfigDefaults();
     }
+
+    /**
+     * --- NEW ---
+     * This method adds all default values to the config in memory.
+     * When paired with copyDefaults(true), it will add any *missing*
+     * keys to the user's config.yml file on disk.
+     */
+    private void syncConfigDefaults() {
+        // Add the version key
+        cfg.addDefault("config-version", LATEST_CONFIG_VERSION);
+
+        // --- Claiming & Limits ---
+        cfg.addDefault("claims.max_claims_per_player", 1);
+        cfg.addDefault("limits.min_radius", 1);
+        cfg.addDefault("limits.max_radius", 32);
+        cfg.addDefault("limits.max_area", 16000);
+        cfg.addDefault("limits.preview_seconds", 10);
+
+        // --- Economy ---
+        cfg.addDefault("use_vault", true);
+        cfg.addDefault("claim_cost", 100.0);
+        cfg.addDefault("item_cost.type", "DIAMOND");
+        cfg.addDefault("item_cost.amount", 5);
+        cfg.addDefault("refund_on_unclaim", false);
+        cfg.addDefault("refund_percent", 0);
+
+        // --- Effects & Visuals ---
+        cfg.addDefault("effects.on_claim.lightning_visual", true);
+        cfg.addDefault("effects.on_claim.particle", "TOTEM");
+
+        // --- Protections ---
+        cfg.addDefault("protections.no_mobs_in_claims", true);
+        cfg.addDefault("protections.pvp_protection", true);
+        cfg.addDefault("protections.container_protection", true);
+
+        // --- Sounds ---
+        cfg.addDefault("sounds.global_enabled", true);
+
+        // --- Admin Options ---
+        cfg.addDefault("admin.auto_remove_banned_plots", false);
+        cfg.addDefault("admin.bypass_claim_limit", false);
+        cfg.addDefault("admin.broadcast_admin_actions", false);
+
+        // --- Merge and Save ---
+        // This copies all the defaults we just added into the in-memory config
+        cfg.options().copyDefaults(true);
+        // This saves the merged config back to disk
+        save();
+
+        // Re-read the config one last time to ensure we are using the saved values
+        plugin.reloadConfig();
+        this.cfg = plugin.getConfig();
+    }
+
 
     /* -----------------------------
      * Claiming & Limits
      * ----------------------------- */
+// ... existing code ...
     public int getMaxClaimsPerPlayer() {
-        return cfg.getInt("claims.max_claims_per_player", 1);
+// ... existing code ...
     }
 
     public int getMinRadius() {
-        return cfg.getInt("limits.min_radius", 1);
+// ... existing code ...
     }
 
     public int getMaxRadius() {
-        return cfg.getInt("limits.max_radius", 32);
+// ... existing code ...
     }
-
-    public int getMaxArea() {
-        return cfg.getInt("limits.max_area", 16000);
-    }
-
+// ... existing code ...
     public int getPreviewSeconds() {
-        return cfg.getInt("limits.preview_seconds", 10);
+// ... existing code ...
     }
 
     /* -----------------------------
      * Economy
      * ----------------------------- */
+// ... existing code ...
     public boolean useVault() {
-        return cfg.getBoolean("use_vault", true);
+// ... existing code ...
     }
 
+_ ... existing code ..._
     public double getClaimCost() {
-        return cfg.getDouble("claim_cost", 100.0);
+// ... existing code ...
     }
-
+// ... existing code ...
     public String getItemCostType() {
-        return cfg.getString("item_cost.type", "DIAMOND");
+// ... existing code ...
     }
 
     public int getItemCostAmount() {
-        return cfg.getInt("item_cost.amount", 5);
+// ... existing code ...
     }
 
     public boolean refundOnUnclaim() {
-        return cfg.getBoolean("refund_on_unclaim", false);
+// ... existing code ...
     }
 
     public int getRefundPercent() {
-        return cfg.getInt("refund_percent", 0);
+// ... existing code ...
     }
 
     /* -----------------------------
      * Effects & Visuals
      * ----------------------------- */
+// ... existing code ...
     public boolean lightningOnClaim() {
-        return cfg.getBoolean("effects.on_claim.lightning_visual", true);
+// ... existing code ...
     }
 
     public String getClaimParticle() {
-        return cfg.getString("effects.on_claim.particle", "TOTEM");
+// ... existing code ...
     }
 
     /* -----------------------------
      * Protections
      * ----------------------------- */
+// ... existing code ...
     public boolean noMobsInClaims() {
-        return cfg.getBoolean("protections.no_mobs_in_claims", true);
+// ... existing code ...
     }
-
+// ... existing code ...
     public boolean pvpProtectionDefault() {
-        return cfg.getBoolean("protections.pvp_protection", true);
+// ... existing code ...
     }
 
     public boolean containerProtectionDefault() {
-        return cfg.getBoolean("protections.container_protection", true);
+// ... existing code ...
     }
 
     /* -----------------------------
      * Sounds
      * ----------------------------- */
+// ... existing code ...
     public boolean globalSoundsEnabled() {
-        return cfg.getBoolean("sounds.global_enabled", true);
+// ... existing code ...
     }
 
     /* -----------------------------
      * Admin Options
      * ----------------------------- */
+// ... existing code ...
     public boolean autoRemoveBannedPlots() {
-        return cfg.getBoolean("admin.auto_remove_banned_plots", false);
+// ... existing code ...
     }
 
     public boolean adminBypassClaimLimit() {
-        return cfg.getBoolean("admin.bypass_claim_limit", false);
+// ... existing code ...
     }
-
+// ... existing code ...
     public boolean broadcastAdminActions() {
-        return cfg.getBoolean("admin.broadcast_admin_actions", false);
+// ... existing code ...
     }
 
     /* -----------------------------
      * Helpers
      * ----------------------------- */
+// ... existing code ...
     public FileConfiguration raw() {
-        return cfg;
+// ... existing code ...
     }
 
     public void save() {
-        plugin.saveConfig();
+// ... existing code ...
     }
 }

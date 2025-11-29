d3559c1c2965891af30f8b6159fef28403cf703c
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-11-29 00:35:28 -0700

    Update Pl3xMapHook.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/hooks/Pl3xMapHook.java
+++ src/main/java/com/aegisguard/hooks/Pl3xMapHook.java
@@ -10,7 +10,7 @@ import net.pl3x.map.core.markers.option.Fill;
 import net.pl3x.map.core.markers.option.Options;
 import net.pl3x.map.core.markers.option.Stroke;
 import net.pl3x.map.core.world.World;
-import net.pl3x.map.Key;
+import net.pl3x.map.core.Key; // Correct for v3.0.0-beta.9
 
 import java.util.Collection;
 

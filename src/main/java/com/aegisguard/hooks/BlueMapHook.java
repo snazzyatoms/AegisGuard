35c28fa9c21dae9e2aa1e1d5eb0127edbb450526
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-12-03 22:34:06 -0700

    Update BlueMapHook.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/hooks/BlueMapHook.java
+++ src/main/java/com/aegisguard/hooks/BlueMapHook.java
@@ -1,8 +1,8 @@
-package com.yourname.aegisguard.hooks;
+package com.aegisguard.hooks;
 
-import com.yourname.aegisguard.AegisGuard;
-import com.yourname.aegisguard.objects.Estate;
-import com.yourname.aegisguard.objects.Guild;
+import com.aegisguard.AegisGuard;
+import com.aegisguard.objects.Estate;
+import com.aegisguard.objects.Guild;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
 import de.bluecolored.bluemap.api.BlueMapWorld;

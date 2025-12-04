fed30db619ec0e58d5f3cfa62d80784ec62ea902
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-12-03 22:06:17 -0700

    Update AdminPlotListGUI.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/gui/AdminPlotListGUI.java
+++ src/main/java/com/aegisguard/gui/AdminPlotListGUI.java
@@ -1,8 +1,8 @@
 package com.aegisguard.gui;
 
-import com.yourname.aegisguard.AegisGuard;
-import com.yourname.aegisguard.managers.LanguageManager;
-import com.yourname.aegisguard.objects.Estate;
+import com.aegisguard.AegisGuard;
+import com.aegisguard.managers.LanguageManager;
+import com.aegisguard.objects.Estate;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;

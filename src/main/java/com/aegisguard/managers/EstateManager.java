8a497a85915216b0f0bc2615e76c7e1cd8938c7f
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-12-04 01:07:12 -0700

    Update EstateManager.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/managers/EstateManager.java
+++ src/main/java/com/aegisguard/managers/EstateManager.java
@@ -1,8 +1,8 @@
-package com.yourname.aegisguard.managers;
+package com.aegisguard.managers;
 
-import com.yourname.aegisguard.AegisGuard;
-import com.yourname.aegisguard.objects.Cuboid;
-import com.yourname.aegisguard.objects.Estate;
+import com.aegisguard.AegisGuard;
+import com.aegisguard.objects.Cuboid;
+import com.aegisguard.objects.Estate;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;

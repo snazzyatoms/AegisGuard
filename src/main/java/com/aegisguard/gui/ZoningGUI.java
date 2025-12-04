bc2bd3c65f42a1fdf58be8f663e72f367ff8b1de
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-12-03 22:14:14 -0700

    Update ZoningGUI.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/gui/ZoningGUI.java
+++ src/main/java/com/aegisguard/gui/ZoningGUI.java
@@ -1,10 +1,10 @@
-package com.yourname.aegisguard.gui;
+package com.aegisguard.gui;
 
-import com.yourname.aegisguard.AegisGuard;
-import com.yourname.aegisguard.economy.CurrencyType;
-import com.yourname.aegisguard.managers.LanguageManager;
-import com.yourname.aegisguard.objects.Estate;
-import com.yourname.aegisguard.objects.Zone;
+import com.aegisguard.AegisGuard;
+import com.aegisguard.economy.CurrencyType;
+import com.aegisguard.managers.LanguageManager;
+import com.aegisguard.objects.Estate;
+import com.aegisguard.objects.Zone;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
